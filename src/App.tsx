ec0a9144606359147c18ee719044050dab318de9
DhakerSalah <dhaker.saleh@clevertech-france.fr>
2023-10-12 22:16:58 +0100

    kanban board project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import KanbanBoard from "./components/KanbanBoard";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <KanbanBoard />;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div
+      className="flex 
+        items-center 
+        justify-center 
+        text-4xl"
+    >
+      Kanban board with <span className="text-rose-500 px-2"> Dhaker SALAH</span>
+    </div>
+  );
+}
+
+export default Header;
